87dfffc5502f725916d1b4c4c97ec781a8b4bec9
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-12 12:26:43 -0300

    creando formulario

--- src/component/Formulario.js
+++ src/component/Formulario.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
 const Formulario = () => {
-    return ( 
-        <h1>Formulario aqui</h1>
-     );
+    return (
+        <form
+        className='col-12'>
+            <fieldset className='text-center'>
+                <legend>Buscar Coctel por Categoria o Ingredientre</legend>
+            </fieldset>
+            <div className='row mt-4'>
+                <div className='col-md-4'>
+                    <input
+                        name='nombre'
+                        className='form-control'
+                        type='text'
+                        placeholder='Buscar por Integrante'
+                    />
+                </div>
+                <div className='col-md-4'>
+                    <select
+                        className='form-control'
+                        name='categoria'
+                    >
+                        <option value=''>--Selecciona Categoria</option>
+                    </select>
+                </div>
+                <div className='col-md-4'>
+                <input
+                        name='submit'
+                        className='btn btn-block btn-primary'
+                        value='Buscar Recetas'
+                    />
+                </div>
+            </div>
+        </form>
+    );
 }
- 
+
 export default Formulario;
