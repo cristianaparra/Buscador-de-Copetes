17ce597d87204654f845ac1ba56fafe3025e8fbd
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-12 12:06:43 -0300

    Creando el inicio de nuestra aplicacion

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+   <header className='bg-alert'>
+       <h1>busca recetas de cocteles o bebidas</h1>
+   </header>
+      );
+}
+ 
+export default Header;
