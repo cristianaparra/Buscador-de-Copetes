7f89f366caa85beeb50d4e499826e846ec657953
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-12 23:11:18 -0300

    importando modal de material-ui y activarlo

--- src/component/Receta.js
+++ src/component/Receta.js
@@ -1,7 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ModalContext } from '../context/ModalContext'
 
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 600,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 const Receta = ({ receta }) => {
+    //configruacion dle modal de material-ui
+    const [modalStyle] = useState(getModalStyle)
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles()
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+    }
 
     //extraer los valores del context
 
@@ -15,12 +51,25 @@ const Receta = ({ receta }) => {
                     <button
                         type='button'
                         className='btn btn-block btn-primary'
-                        onClick={()=>{
-                            guardarIdReceta(receta.idDrink)
+                        onClick={() => {
+                            guardarIdReceta(receta.idDrink);
+                            handleOpen()
                         }}>
-                            
+
                         Ver Receta
                     </button>
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            guardarIdReceta(null)
+                            handleClose()
+                        }}
+                    >
+                        <div style={modalStyle}
+                            className={classes.paper}>
+                            <h1>modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
